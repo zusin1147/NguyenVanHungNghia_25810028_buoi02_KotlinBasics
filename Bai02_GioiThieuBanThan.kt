e71e53802238babddab583a2b9e6cee02e7c79be
zusin1147 <yowai1642003@gmail.com>
2026-09-24 01:44:49 +0700

    Buoi02 Bai02 gioi thieu ban than

---
+++ Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,11 @@
+// Họ tên: Nguyễn Văn Hùng Nghĩa - MSSV: 25810028
+
+fun main() {
+    val hoTen: String = "Nguyễn Văn Hùng Nghĩa"
+    val namSinh: Int = 2003
+    val namHienTai: Int = 2026
+
+    val tuoi: Int = namHienTai - namSinh
+
+    println("Xin chào thầy và các bạn, em tên là $hoTen, sinh năm $namSinh, năm nay em $tuoi tuổi.")
+}
