bfb21a6181d5919eb75e82a359577def25532960
zusin1147 <yowai1642003@gmail.com>
2026-09-24 12:17:49 +0700

    Buoi02 Bai03 tai khoan ngan hang don gian

---
+++ Bai03_TaiKhoanNganHang.kt
@@ -0,0 +1,15 @@
+// Họ tên: Nguyễn Văn Hùng Nghĩa - MSSV: 25810028
+
+fun main() {
+    val soDuBanDau: Double = 5000000.0
+    var soDu: Double = soDuBanDau
+    println("So du ban dau: $soDuBanDau VND")
+
+    val tienGui: Double = 2000000.0
+    soDu += tienGui
+    println("Gui them: $tienGui VND -> So du hien tai: $soDu VND")
+
+    val tienRut: Double = 1500000.0
+    soDu -= tienRut
+    println("Rut ra: $tienRut VND -> So du hien tai: $soDu VND")
+}
